048df48a027d81f68215996986e544ab69571c7c
Mauricio Rojas Barrientos <maalroja@bancolombia.com.co>
2023-05-23 11:17:00 -0400

    chore: get movie by id

--- main.go
+++ main.go
@@ -25,6 +25,29 @@ var movies []Movie
 
 func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode(movies)
+
+}
+
+func deleteMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...) // Remove movie by id
+			break
+		}
+	}
 	json.NewEncoder(w).Encode(movies)
 }
 
@@ -34,7 +57,9 @@ func main() {
 	movies = append(movies, Movie{ID: "2", Isbn: "999923213", Title: "El imperio contra ataca", Director: &Director{Firstname: "George", Lastname: "Lukas"}})
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovies).Methods("GET")
-
+	//r.HandleFunc("/movies", createMovies).Methods("POST")
+	//r.HandleFunc("/movies/{id}", updateMovies).Methods("PUT")
+	//r.HandleFunc("/movies/{id}", deleteMovies).Methods("DELETE")
 	fmt.Printf("Starting server at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
